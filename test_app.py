b625aeeb0d23de6d25785d9d6012adb46116cf83
learningsiva <siva.learnnig@gmail.com>
2026-04-24 16:50:42 +0530

    new test cases along requirements

--- test_app.py
+++ test_app.py
@@ -1,4 +1,10 @@
 from app import add
 
-def test_add():
+def test_add_positive():
     assert add(2, 3) == 5
+
+def test_add_zero():
+    assert add(0, 5) == 5
+
+def test_add_negative():
+    assert add(-1, -1) == -2
